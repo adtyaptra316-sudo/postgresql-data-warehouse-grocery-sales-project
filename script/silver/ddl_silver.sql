224b220b72c66aed3e400192348cfa015b5b7056
Aditya Eka Putra <adtyaptra316@gmail.com>
2026-07-28 14:35:48 +0700

    Update ddl_silver.sql

--- script/silver/ddl_silver.sql
+++ script/silver/ddl_silver.sql
@@ -20,14 +20,16 @@ create table silver.clean_products(
 	modify_date timestamp,
 	resistant varchar(15),
 	is_alergant varchar(15),
-	vitality_days int		-- change data type from varchar(15) to integer
+	vitality_days int,		-- change data type from varchar(15) to integer
+	dwh_created_date timestamp default current_timestamp		-- create technical column for ETL process load timestamp
 );
 
 
 drop table if exists silver.clean_categories;
 create table silver.clean_categories(
 	category_id int,
-	category_name varchar(100)
+	category_name varchar(100),
+	dwh_created_date timestamp default current_timestamp		-- create technical column for ETL process load timestamp
 );
 
 
@@ -39,7 +41,8 @@ create table silver.clean_customers(
 	last_name varchar(50),
 	full_name varchar(104),			-- create derived column full name
 	city_id int,
-	addres varchar(255)
+	addres varchar(255),
+	dwh_created_date timestamp default current_timestamp		-- create technical column for ETL process load timestamp
 );
 
 
@@ -47,7 +50,8 @@ drop table if exists silver.clean_countries;
 create table silver.clean_countries(
 	country_id int,
 	country_name varchar(50),
-	country_code varchar(2)
+	country_code varchar(2),
+	dwh_created_date timestamp default current_timestamp		-- create technical column for ETL process load timestamp
 );
 
 
@@ -56,7 +60,8 @@ create table silver.clean_cities(
 	city_id int,
 	city_name varchar(50),
 	zipcode varchar (10),
-	country_id int
+	country_id int,
+	dwh_created_date timestamp default current_timestamp		-- create technical column for ETL process load timestamp
 );
 
 
@@ -70,7 +75,8 @@ create table silver.clean_employees(
 	birth_date timestamp,
 	gender varchar(10),
 	city_id int,
-	hire_date timestamp
+	hire_date timestamp,
+	dwh_created_date timestamp default current_timestamp		-- create technical column for ETL process load timestamp
 );
 
 
@@ -84,5 +90,6 @@ create table silver.clean_sales(
 	discount decimal(10,2),
 	total_price decimal(12,4),
 	sales_date timestamp,
-	transaction_number varchar(255)
+	transaction_number varchar(255),
+	dwh_created_date timestamp default current_timestamp		-- create technical column for ETL process load timestamp
 );
